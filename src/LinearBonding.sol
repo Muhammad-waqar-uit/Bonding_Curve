7cb1329521ef07252464d97b36dc0e59dccf9d3d
Muhammad Waqar <MW6069605@GMAIL.COM>
2023-02-09 12:22:46 +0500

    linear-bonding-curve

--- src/LinearBonding.sol
+++ src/LinearBonding.sol
@@ -1,76 +1,137 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TokenBondingCurve_Linear is ERC20, Ownable {
-    uint256 private _tax;
+contract BondingCurveToken is ERC20, Ownable {
+    uint256 private _loss;
+
     uint256 private immutable _slope;
 
-    uint256 private constant LOSS_FEE_PERCENTAGE = 1000;
+    // The percentage of loss when selling tokens (using two decimals)
+    uint256 private constant _LOSS_FEE_PERCENTAGE = 1000;
 
-    constructor(string memory name_, string memory symbol_, uint slope_)
-        ERC20(name_, symbol_)
-    {
+    /**
+     * @dev Constructor to initialize the contract.
+     * @param name_ The name of the token.
+     * @param symbol_ The symbol of the token.
+     * @param slope_ The slope of the bonding curve.
+     */
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint slope_
+    ) ERC20(name_, symbol_) {
         _slope = slope_;
     }
 
+    /**
+     * @dev Allows a user to buy tokens.
+     * @param _amount The number of tokens to buy.
+     */
     function buy(uint256 _amount) external payable {
-        uint256 price = _calculatePriceForBuy(_amount);
-        require(msg.value >= price, "Low on Ether");
+        uint price = _calculatePriceForBuy(_amount);
+        require(msg.value >= price, "Not enough Ether to buy tokens");
         _mint(msg.sender, _amount);
-        (bool sent,) = payable(msg.sender).call{value: msg.value - price}("");
-        require(sent, "Failed to send Ether");
+        payable(msg.sender).transfer(msg.value - price);
     }
 
+    /**
+     * @dev Allows a user to sell tokens at a 10% loss.
+     * @param _amount The number of tokens to sell.
+     */
     function sell(uint256 _amount) external {
-        require(balanceOf(msg.sender) >= _amount, "Low on Tokens");
-        uint256 price = _calculatePriceForSell(_amount);
-        uint256 tax = _calculateLoss(price);
+        require(balanceOf(msg.sender) >= _amount, "Not enough tokens to sell");
+        uint256 _price = _calculatePriceForSell(_amount);
+        uint tax = _calculateLoss(_price);
         _burn(msg.sender, _amount);
-        _tax += tax;
-        payable(msg.sender).transfer(price - tax);
+        _loss += tax;
+
+        payable(msg.sender).transfer(_price - tax);
     }
 
+    /**
+     * @dev Allows the owner to withdraw the lost ETH.
+     */
     function withdraw() external onlyOwner {
-        require(_tax > 0, "Low on Ether");
-        uint256 amount = _tax;
-        _tax = 0;
+        require(_loss > 0, "No ETH to withdraw");
+        uint amount = _loss;
+        _loss = 0;
         payable(owner()).transfer(amount);
     }
 
-    function getCurrentPrice() external view returns (uint256) {
+    /**
+     * @dev Returns the current price of the token based on the bonding curve formula.
+     * @return The current price of the token in wei.
+     */
+    function getCurrentPrice() external view returns (uint) {
         return _slope * totalSupply();
     }
 
-    function calculatePriceForBuy(uint256 _tokensToBuy) external view returns (uint256) {
+    /**
+     * @dev Returns the price for buying a specified number of tokens.
+     * @param _tokensToBuy The number of tokens to buy.
+     * @return The price in wei.
+     */
+    function calculatePriceForBuy(
+        uint256 _tokensToBuy
+    ) external view returns (uint256) {
         return _calculatePriceForBuy(_tokensToBuy);
     }
 
-    function calculatePriceForSell(uint256 _tokensToSell) external view returns (uint256) {
+    /**
+     * @dev Returns the price for selling a specified number of tokens.
+     * @param _tokensToSell The number of tokens to sell.
+     * @return The price in wei.
+     */
+    function calculatePriceForSell(
+        uint256 _tokensToSell
+    ) external view returns (uint256) {
         return _calculatePriceForSell(_tokensToSell);
     }
 
-    function _calculatePriceForBuy(uint256 _tokensToBuy) private view returns (uint256) {
-        uint256 price = 0;
-        uint256 totalSupply = totalSupply();
-        for (uint i = totalSupply + 1; i < totalSupply + _tokensToBuy + 1; i++) {
-            price += i * _slope;
-        }
-        return price;
+    /**
+     * @dev Calculates the price for buying a certain number of tokens based on the bonding curve formula.
+     * @param _tokensToBuy The number of tokens to buy.
+     * @return The price in wei for the specified number of tokens.
+     */
+    function _calculatePriceForBuy(
+        uint256 _tokensToBuy
+    ) private view returns (uint256) {
+        return
+            (_slope *
+                (_tokensToBuy *
+                    (_tokensToBuy + 1) +
+                    2 *
+                    totalSupply() *
+                    _tokensToBuy)) / 2;
     }
 
-    function _calculatePriceForSell(uint256 _tokensToSell) private view returns (uint256) {
-        uint256 price = 0;
-        uint256 totalSupply = totalSupply();
-        for (uint i = totalSupply - _tokensToSell + 1; i <= totalSupply; i++) {
-            price += i * _slope;
+    /**
+     * @dev Calculates the price for selling a certain number of tokens based on the bonding curve formula.
+     * @param _tokensToSell The number of tokens to sell.
+     * @return The price in wei for the specified number of tokens
+     */
+    function _calculatePriceForSell(
+        uint256 _tokensToSell
+    ) private view returns (uint256) {
+        uint totalSupply = totalSupply();
+        if (_tokensToSell > totalSupply) {
+            _tokensToSell = totalSupply;
         }
-        return price;
+        return
+            _slope *
+            ((_tokensToSell * (totalSupply + totalSupply - _tokensToSell + 1)) /
+                2);
     }
 
-    function _calculateLoss(uint256 price) private pure returns (uint256) {
-        return price / 10;
+    /**
+     * @dev Calculates the loss for selling a certain number of tokens.
+     * @param amount The price of the tokens being sold.
+     * @return The loss in wei.
+     */
+    function _calculateLoss(uint256 amount) private pure returns (uint256) {
+        return (amount * _LOSS_FEE_PERCENTAGE) / (1E4);
     }
 }
